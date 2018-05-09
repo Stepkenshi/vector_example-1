710483a18afb2dee9cf3db3bf593e297d3f525a9
Stepkenshi <37145587+Stepkenshi@users.noreply.github.com>
2018-05-09 18:45:31 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -3,57 +3,118 @@
 
 #include "vector.hpp"
 
-vector_t::vector_t()
+vector_t::vector_t() : elements_(nullptr), size_(0), capacity_(0)
 {
 }
 
 vector_t::vector_t(vector_t const & other)
 {
+    	elements_ = new int[other.capacity_];
+    	for (std::size_t i = 0; i < other.size_; i++)
+    	{
+        	elements_[i] = other.elements_[i];
+    	}
+    	capacity_ = other.capacity_;
+	size_ = other.size_;
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
-	return *this;
+	if (this != other)
+ 	{
+		elememnts_[i] = other.elements_[i];
+		delete[]elements_;
+		elements_ = new int [other.capacity_];
+
+		for (size_t i = 0; i < other.capacity_; i++)
+		{
+			elememnts_[i] = other.elements_[i];
+		}
+		capacity_ = other.capacity_;
+		size_ = other.size_;
+		return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	return false;
+	if (size_ == other.size_)
+	{
+		for (size_t i = 0; i < other.size_; i++)
+		{
+			if (elements_[i] != other.elements_[i])
+			{
+				return false;
+				break;
+			}
+		}
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 vector_t::~vector_t()
 {
+	if(elements_ != nullptr)
+	{
+		delete[]elements_;
+	}
 }
 
 std::size_t vector_t::size() const
 {
-    return 0;
+    return size_;
 }
 
 std::size_t vector_t::capacity() const
 {
-    return 0;
+    return capacity_;
 }
 
 void vector_t::push_back(int value)
 {
-}
-
+	if (capacity_)
+	{
+		if ( size_ == capacity_)
+ 	{
+ 		capacity_ *= 2;
+		int memory = new int [ele
+		int memory = new int [capacity_];
+		for (size_t i = 0; i < capacity_; i++)
+		{
+			memory[i] = 0;
+		}
+		for (size_t i = 0; i < capacity_; i++)
+		{
+			elements_[i] = value;
+		}
+		
+		
+ 	}
+ 		
+ }
 void vector_t::pop_back()
-{
-}
+ {
+	if (size_ == 0)
+	{
+		return *this;
+	}
+	
+ }
 
 int & vector_t::operator [](std::size_t index)
 {
-	return elements_[0];
+	return elements_[index];
 }
 
 int vector_t::operator [](std::size_t index) const
 {
-	return 0;
+	return elements_[index];
 }
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs)
 {
-	return true;
+	return !(lhs == rhs);
 }
